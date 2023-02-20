1f9188d7311890106c42dfb32e5fefd58ab72c31
KarimMoustamid <karim.moustamid@agilethought.com>
2023-02-20 10:15:34 -0600

    Adding Css

--- TangyLibrary.Models/LearnBlazorModels/Demo_ProductProp.cs
+++ TangyLibrary.Models/LearnBlazorModels/Demo_ProductProp.cs
@@ -4,5 +4,5 @@ public class Demo_ProductProp
 {
     public int Id { get; set; }
     public string Key { get; set; }
-    public string Value { get; set; }
+    public string Value { get; set; } 
 }
